2d929a1e528d5cfd0ae21327fbeb6c4212c2693d
Quinton Wall <qwall@salesforce.com>
2016-06-13 09:43:07 -0700

    updated callback url

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -10,7 +10,7 @@ let nforce = require('nforce'),
 let org = nforce.createConnection({
     clientId: SF_CLIENT_ID,
     clientSecret: SF_CLIENT_SECRET,
-    redirectUri: 'http://https://dreamhouse-messenger-bot.herokuapp.com/oauth/_callback',
+    redirectUri: 'https://dreamhouse-messenger-bot.herokuapp.com/oauth/_callback',
     mode: 'single',
     autoRefresh: true
 });
